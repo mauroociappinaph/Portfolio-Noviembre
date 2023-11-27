38b51a01e0ab8d492d6e75a24f633afb7bd9f1df
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-11-26 21:13:28 -0300

    Skills page

---
+++ portfolio/src/Componentes/skills.jsx
@@ -0,0 +1,15 @@
+/*
+Tu background como comunicador audiovisual te da una serie de habilidades que son muy valiosas en el campo de la programación, tanto front-end como back-end. Estas habilidades incluyen:
+
+SKILLS
+
+Creatividad: Como comunicador audiovisuales soy expertos en crear contenido visual y multimedia que sean atractivos. Lo que me permite desarrollo de interfaces de usuario (UI) y experiencias de usuario (UX) efectivas.
+
+Comunicación: Como comunicador audiovisuales soy expertos en comunicar ideas de manera clara y concisa. Esta habilidad es esencial para la documentación de código y la comunicación con otros desarrolladores.
+
+Problema-solving: Como comunicador audiovisual estoy acostumbrados a pensar fuera de la caja y encontrar soluciones creativas a los problemas. Una habilidad la cual aplico para el desarrollo de software eficaz.
+
+SOFT-SKILLS
+
+
+*/
