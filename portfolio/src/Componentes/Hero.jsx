e4b447e1ef5570eaf52c7cf87eee23be8a49660f
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-11-29 14:46:03 -0300

    Sin animación el Hero

--- portfolio/src/Componentes/Hero.jsx
+++ portfolio/src/Componentes/Hero.jsx
@@ -1,93 +1,50 @@
-import React, { useEffect } from "react";
-import { motion, useAnimation } from "framer-motion";
+import React from "react";
 import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 import img1 from "../asset/vecteezy_3d-male-character-engaged-in-work-on-a-laptop_24658981_575.png";
 
 const Hero = () => {
-  const controls = useAnimation();
-
-  useEffect(() => {
-    controls.start("visible");
-  }, [controls]);
-
-  const headerVariants = {
-    hidden: { opacity: 0, y: -20 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: { duration: 0.6, ease: "easeOut" },
-    },
-  };
-
-  const iconVariants = {
-    hidden: { scale: 0 },
-    visible: {
-      scale: 1,
-      transition: { duration: 0.6, ease: "easeOut" },
-    },
-  };
-
   return (
-    <div className=" py-16 px-4 sm:px-6 lg:px-8 relative bg-secondary  ">
+    <div className="py-16 px-4 sm:px-6 lg:px-8 relative bg-secondary">
       <div className="max-w-screen-xl mx-auto relative z-10">
         <div className="lg:flex lg:items-center lg:justify-between flex-wrap">
           <div className="w-full lg:w-1/2 min-w-0 mb-6 lg:mb-0">
-            <motion.div
-              className="mb-4"
-              initial="hidden"
-              animate={controls}
-              variants={headerVariants}
-            >
-              <motion.div className="text-white text-4xl  sm:text-5xl md:text-6xl">
+            <div className="mb-4">
+              <div className="text-white text-4xl  sm:text-5xl md:text-6xl">
                 Mauro José <br />
-                <span className=" font-extrabold text-7xl">Ciappina</span>
-              </motion.div>
-              <motion.div className="text-white text-lg sm:text-xl md:text-2xl my-4">
+                <span className="font-extrabold text-7xl">Ciappina</span>
+              </div>
+              <div className="text-white text-lg sm:text-xl md:text-2xl my-4">
                 Full Stack Developer
-              </motion.div>
-            </motion.div>
-            <motion.div
-              className="mt-8"
-              initial="hidden"
-              animate={controls}
-              variants={headerVariants}
-            >
-              <motion.div className="flex space-x-4">
-                <motion.a
+              </div>
+            </div>
+            <div className="mt-8">
+              <div className="flex space-x-4">
+                <a
                   href="https://www.linkedin.com/in/maurojoseciappina/"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-white transition-transform transform hover:scale-110"
-                  initial="hidden"
-                  animate={controls}
-                  variants={iconVariants}
+                  className="text-white"
                 >
                   <FaLinkedin size={40} />
-                </motion.a>
-                <motion.a
+                </a>
+                <a
                   href="https://github.com/mauroociappinaph"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-white transition-transform transform hover:scale-110"
-                  initial="hidden"
-                  animate={controls}
-                  variants={iconVariants}
+                  className="text-white"
                 >
                   <FaGithub size={40} />
-                </motion.a>
-                <motion.a
+                </a>
+                <a
                   href="https://twitter.com/mauroo_jose"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-white transition-transform transform hover:scale-110"
-                  initial="hidden"
-                  animate={controls}
-                  variants={iconVariants}
+                  className="text-white"
                 >
                   <FaTwitter size={40} />
-                </motion.a>
-              </motion.div>
-            </motion.div>
+                </a>
+              </div>
+            </div>
           </div>
 
           {/* Aquí se agrega la imagen a la derecha */}
