db8f01aab2443b7b27aa2204bd0882f7670f7065
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-12-06 13:36:10 -0300

    Eliminación de CV

--- portfolio/src/Componentes/Nav.jsx
+++ portfolio/src/Componentes/Nav.jsx
@@ -1,17 +1,10 @@
 import Logo from "../../src/asset/logo/Logo.png";
 
 const Nav = () => {
-  const CV = "../asset/cve.pdf";
-
   return (
     <nav className="flex items-center justify-between p-4 bg-primary">
-      <a
-        href={CV}
-        download
-        className="px-7 py-3 md:px-9 md:py-4 bg-primary font-medium md:font-semibold text-white text-lg uppercase  rounded-md hover:bg-secondary hover:text-white transition ease-linear duration-500 flex items-center"
-      >
-        Get my CV
-      </a>
+      <img src={Logo} alt="Logo" className="w-auto h-28" />{" "}
+      {/* Ajusta el tamaño del logo */}
     </nav>
   );
 };
