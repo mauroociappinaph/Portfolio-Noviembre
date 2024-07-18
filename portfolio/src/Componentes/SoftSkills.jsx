d1b02519794920396c278b9b225fcd692169d938
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2024-07-18 13:42:49 -0300

    chore: Update SoftSkills component styling

--- portfolio/src/Componentes/SoftSkills.jsx
+++ portfolio/src/Componentes/SoftSkills.jsx
@@ -20,14 +20,14 @@ const skills = [
 
 const Softskills = () => {
   return (
-    <div className="p-4 md:lg:xl:p-8 bg-secondary py-8">
+    <div className="p-4 md:p-8 bg-secondary py-8">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-10">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl text-center font-bold text-white mb-6 col-span-3">Soft Skills</h1>
+        <h1 className="text-4xl md:text-5xl lg:text-6xl text-center font-bold text-white mb-6 col-span-1 md:col-span-2 lg:col-span-3">Soft Skills</h1>
         {skills.map((skill, index) => (
           <div key={index} className="p-4 md:p-6 lg:p-8 flex flex-col items-center text-center bg-white rounded-lg shadow-md mb-4">
             <span className="p-4 md:p-5 rounded-full bg-primary text-white shadow-lg shadow-red-200">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-8 md:h-10 w-8 md:w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1.5">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
               </svg>
             </span>
             <p className="text-lg md:text-xl lg:text-2xl font-medium text-slate-700 mt-3">{skill.title}</p>
